5e695c89af7195341e54028da5dff09fb82e2820
BestOreo <geshuaiqi001@gmail.com>
2018-06-01 13:35:25 +0800

    return with value

--- RGenerator.cpp
+++ RGenerator.cpp
@@ -153,149 +153,18 @@ public:
 	void releaseLabel(int i){
 			labels[i]=false;
 	}
+
+
 	string stratTranslate(node* root,Data* r) {
-		node* t = root;
-		string value="";
-		while (t) {
-            if(t->type.compare("function_definition")==0){
-                cout << "FUNCTION" << t->contents[1]->content << "" << endl;
-                loop(t,r);
-
-            }
-
-            else if(t->type.compare("parameter_declaration")==0){
-                cout << "PARAM " <<  t->contents[1]->content << endl;
-                loop(t,r);
-            }
-            else if (t->type.compare("declaration")==0){
-				string ttype = t->sub[0]->contents[0]->content;
-                for(int i=0;i<t->sub[1]->sub.size();i++){
-					node* tt = t->sub[1]->sub[i];
-					//cout<<"debug----"<<tt->contents[0]->content<<endl;
-					r->setVar(tt->contents[0]->content,ttype);
-					loop(tt,r);
-				}
-            }
-			else if (t->type.find("expression")!=-1&&t->type.compare("conditional_expression2")!=0){
-				node* tt=t->sub[0];
-				string r1,r2,r3,l;
-                if(tt->sub.size()==0){
-					if(tt->contents[0]->name.compare("IDENTIFIER")==0)
-						r1 = string("var").append(itoa(r->getVar(tt->contents[0]->content)));
-					else if(tt->contents[0]->name.compare("CONSTANT")==0){
-						int i = r->getTemp();
-						cout<<"Temp"<<itoa(i)<<" = "<<"#"<<tt->contents[0]->content<<endl;
-						r1 = string("Temp").append(itoa(i));
-					}						
-                }
-				else
-                	r1 = stratTranslate(tt,r);  
-				
-		
-				tt=t->sub[2];
-				if(tt->sub.size()==0){
-					if(tt->contents[0]->name.compare("IDENTIFIER")==0)
-						r3 = string("var").append(itoa(r->getVar(tt->contents[0]->content)));
-					else if(tt->contents[0]->name.compare("CONSTANT")==0){
-						int i = r->getTemp();
-						cout<<"Temp"<<itoa(i)<<" = "<<"#"<<tt->contents[0]->content<<endl;
-						r3 = string("Temp").append(itoa(i));
-					}						
-                }
-				else
-                	r3 = stratTranslate(tt,r);
-
-				tt = t->sub[1];
-				r2 = tt->contents[0]->content;
-				if(tt->contents[0]->name.compare("assignment_operator")==0){
-					cout<<r1<<" "<<r2<<" "<<r3<<endl;
-					value = r1;
-				}
-				else{
-					l = string("Temp").append(itoa(r->getTemp()));
-					value = l;
-					cout<< l<<" = "<<r1<<" "<<r2<<" "<<r3<<endl;
-				}
-				         
-            }
-
-			else if(t->type == "if_statement"){
-				string resReg = stratTranslate(t->sub[0],r);
-				int label_true = getLabel();
-				int label_false = getLabel();
-				cout << "IF " << resReg << " == 0 GOTO label_" << label_true << endl;
-				cout << "GOTO label_" << label_false << endl;
-				cout << "LABEL label_" << label_true<<":" << endl;
-				stratTranslate(t->sub[1],r);
-				cout << "LABEL label_" << label_false <<":" << endl;
-			}
-
-
-			else if(t->type == "if_else_statement"){
-				int label_true = getLabel();
-				int label_false = getLabel();
-				int label_next = getLabel();
-				string resReg = stratTranslate(t->sub[0],r);
-				cout << "IF " << resReg << " == 0 GOTO label_" << label_true << endl;
-				cout << "GOTO label_" << label_false << endl;
-				cout << "LABEL label_" << label_true<<":" << endl;
-				stratTranslate(t->sub[1],r);
-				cout << "GOTO label_" << label_next << endl;
-				cout << "LABEL label_" << label_false <<":" << endl;
-				stratTranslate(t->sub[2],r);
-				cout << "LABEL label_" << label_next <<":" << endl;
-			}
-
-
-			else if(t->type == "while_statement"){
-				int label_while = getLabel();
-				int label_start = getLabel();
-				int label_end = getLabel();
-				cout << "LABEL label_" << label_while <<":" << endl;
-				string resReg = stratTranslate(t->sub[0],r);
-				cout << "IF " << resReg << " == 0 GOTO label_" << label_start << endl;
-				cout << "GOTO label_" << label_end << endl;
-				cout << "LABEL label_" << label_start <<":" << endl;
-				stratTranslate(t->sub[1],r);
-				cout << "GOTO label_" << label_while << endl;
-				cout << "LABEL label_" << label_end <<":" << endl;
-			}
-
-			else if(t->type == "do_while_statement"){
-				int label_while = getLabel();
-				cout << "LABEL label_" << label_while <<":" << endl;
-				stratTranslate(t->sub[0],r);
-				string resReg = stratTranslate(t->sub[1],r);
-				cout << "IF " << resReg << " == 0 GOTO label_" << label_while << endl;
-			}
-
-			else if(t->type == "for_statement_exp3"){
-				int label_for = getLabel();
-				int label_start = getLabel();
-				int label_end = getLabel();
-				stratTranslate(t->sub[0],r);
-				cout << "LABEL label_" << label_for <<":" << endl;
-				string resReg = stratTranslate(t->sub[1],r);
-				cout << "IF " << resReg << " == 0 GOTO label_" << label_start << endl;
-				cout << "GOTO label_" << label_end << endl;
-				cout << "LABEL label_" << label_start <<":" << endl;
-				stratTranslate(t->sub[3],r);
-				stratTranslate(t->sub[2],r);
-				cout << "GOTO label_" << label_for << endl;
-				cout << "LABEL label_" << label_end <<":" << endl;
-			}
-
-			t = t->next; 
-		}
-		return value;
-			
-	}        
+		return stratTranslate(root, *r);
+	}      
 	  
 
     string stratTranslate(node* root,Data r) {
 		node* t = root;
 		string value="";
 		while (t) {
+			
             if(t->type.compare("function_definition")==0){
                 cout << "FUNCTION " << t->contents[1]->content << " :" << endl;
 				if(t->contents[1]->content.find("main")!= -1){
@@ -346,10 +215,21 @@ public:
 						int i = r.getTemp();
 						cout<<"Temp"<<itoa(i)<<" = "<<"#"<<tt->contents[0]->content<<endl;
 						r3 = string("Temp").append(itoa(i));
-					}						
+					}			
                 }
-				else
-                	r3 = stratTranslate(tt,new Data(r));
+				else{
+					if(tt->type == "function_call"){
+						stratTranslate(tt,r);
+						int i = r.getTemp();
+						cout << "Temp" << i << " := CALL " << tt->sub[0]->contents[0]->content << endl;
+						r3 = string("Temp") + to_string(i);
+						r.releaseTemp(i);
+					}
+					else{
+						r3 = stratTranslate(tt,new Data(r));
+					}
+                	//
+				}
 
 				tt = t->sub[1];
 				r2 = tt->contents[0]->content;
@@ -456,7 +336,7 @@ public:
 					
 				}
 				cout << "CALL " << t->sub[0]->contents[0]->content << endl;
-				r.releaseARG();
+				//r.releaseARG();
 			}
 
 			else if(t->type == "return_statement"){
--- test.c
+++ test.c
@@ -1,13 +1,7 @@
-float test()
-{
-	int b=1;
-	b += 2;
-}
-
 
 void main()
 {
 	int c;
-	test(1,c,c+1*4*c);
-	return 1+2*c;
+	c = 3;
+	c = test(1,c*2);
 }
