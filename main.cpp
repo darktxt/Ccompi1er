0c5fd93abe83eb5153c47cce7ab1a2c90fd42959
txt <845663617@qq.com>
2018-06-03 16:34:01 +0800

    merge

--- main.cpp
+++ main.cpp
@@ -12,16 +12,10 @@ int node::ID = 0;
 int main()
 {
 	yyin = fopen("test.c","r");
-	yyparse();
-<<<<<<< HEAD
-	
+	yyparse();	
 	cout<<endl<<endl;
 	
-=======
-	root->debugInfo(true,true); //show info all the instructions following,no extand
-	cout<<endl<<endl;
-	root->next->next->debugInfo(false,true);//only show this instruction,extand
->>>>>>> 080b276d7c191802d11d83f8bde3b099f39c7ab5
+
 	if(!hasError){
 		streambuf* coutBuf = cout.rdbuf(); 
 		ofstream of("tree.txt"); 
